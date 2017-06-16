fd4b5d05a3e034ed09d5abe31c47d432c0cc8ad3
VB <vb>
2017-06-16 12:34:43 +0300

    Lesson 13 Initial commit.

---
+++ Users/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Users.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Login Name")]
+        public string LoginName { get; set; }
+
+        [Display(Name = "Your Real Name")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
---
+++ Users/Models/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Users.Models
+{
+    public static class UserRepository
+    {
+        private static List<User> _allUsers;
+
+        static UserRepository()
+        {
+            _allUsers = new List<User>
+            {
+                new User {Id = 1, BirthDate = new DateTime(1983, 10, 12), Country = "USA", Email = "usa@usa.com", LoginName = "usa", UserName = "Steve"},
+                new User {Id = 2, BirthDate = new DateTime(2000, 3, 8), Country = "Ukraine", Email = "ukraine@ukraine.com", LoginName = "ukraine", UserName = "Oleg"},
+                new User {Id = 3, BirthDate = new DateTime(1999, 1, 1), Country = "UK", Email = "uk@uk.com", LoginName = "uk", UserName = "Lee"}
+            };
+        }
+        public static List<User> AllUsers { get { return _allUsers; } }
+    }
+}
